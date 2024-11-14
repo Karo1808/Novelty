a80e6bfb8b3f86c8ab01298070d30a7ab1e809f6
Karol <106309115+Karo1808@users.noreply.github.com>
2024-11-14 15:57:44 +0100

    fix: specify output direcotry

--- packages/api/src/index.js
+++
@@ -1,9 +0,0 @@
-import { serve } from "@hono/node-server";
-import app from "./app";
-// import env from "./env";
-const port = 3000;
-console.log(`Server is running on port http://localhost:${port}`);
-serve({
-  fetch: app.fetch,
-  port,
-});
