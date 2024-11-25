444c74c9bc252669179d84f0b64548ed9ddd956f
Karol <106309115+Karo1808@users.noreply.github.com>
2024-11-25 16:33:14 +0100

    feat(monitoring): integrate grafana and prometheus

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,9 +3,12 @@ import { healthcheckRouter } from "$/healhcheck/healthcheck.index";
 import configureOpenAPI from "@/lib/configure-open-api";
 import createApp from "@/lib/create-app";
 
+import { configurePrometheus } from "./lib/configure-prometheus";
+
 const app = createApp();
 
 configureOpenAPI(app);
+configurePrometheus(app);
 
 const routes = [healthcheckRouter];
 
