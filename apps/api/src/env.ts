3284e9f90252208169f54bd293041b0e964c22e6
Karol <karolp2003@gmail.com>
2024-12-02 14:01:55 +0100

    chore(file-structure): create a monitoring package to better colocate code

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -18,15 +18,12 @@ const EnvSchema = z.object({
     .enum(["test", "debug", "development", "production"])
     .default("development"),
   PORT: z.coerce.number().default(3001),
+  LOKI_PORT: z.coerce.number().default(3100),
+  NODE_EXPORTER_PORT: z.coerce.number().default(9100),
   LOG_LEVEL: z
     .enum(["fatal", "error", "warn", "info", "debug", "trace", "silent"])
     .default("info"),
   BASE_URL: z.string().default("http://localhost"),
-  PROMETHEUS_PORT: z.coerce.number().default(9090),
-  GRAFANA_PORT: z.coerce.number().default(3000),
-  NODE_EXPORTER_PORT: z.coerce.number().default(9100),
-  LOKI_PORT: z.coerce.number().default(3100),
-  PROMTAIL_PORT: z.coerce.number().default(9080),
   SENTRY_DSN: z.string().default(""),
   IS_OPEN_API_GENERATE: z.coerce.number().default(0),
 });
