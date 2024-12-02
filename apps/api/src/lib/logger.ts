008dc1838ef6ccf248e2fd61516781cb7dc92a04
Karol <106309115+Karo1808@users.noreply.github.com>
2024-12-02 14:01:55 +0100

    chore(file-structure): create a monitoring package to better colocate code

--- apps/api/src/lib/logger.ts
+++ apps/api/src/lib/logger.ts
@@ -1,37 +1,14 @@
-import { pino } from "pino";
-
 import env from "@/env";
+import { configureLogger } from "@novelty/lib/logger";
 
-const transports: any[] = [];
-
-if (env.NODE_ENV !== "production") {
-  transports.push({
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      translateTime: true,
-      level: env.LOG_LEVEL ?? "info",
-    },
-  });
-}
-
-if (env.NODE_ENV !== "test") {
-  transports.push({
-    target: "pino-loki",
-    options: {
-      batching: true,
-      interval: 5,
-      host: `${env.BASE_URL}:${env.LOKI_PORT}`,
-      labels: {
-        application: "api",
-        environment: env.NODE_ENV,
-      },
-    },
-  });
-}
-
-const logger = pino({
-  transport: transports.length > 0 ? { targets: transports } : undefined,
+const logger = configureLogger({
+  nodeEnvironment: env.NODE_ENV ?? "development",
+  hostUrl: `${env.BASE_URL}:${env.LOKI_PORT}`,
+  labels: {
+    source: "api",
+    enviornment: env.NODE_ENV,
+  },
+  logLevel: env.LOG_LEVEL ?? "info",
 });
 
 export default logger;
