5aeeaf1ba05be334651df1af22aa8461a2f77dde
Karol <106309115+Karo1808@users.noreply.github.com>
2024-12-04 16:27:03 +0100

    feat(databae): add utility functions to db package

---
+++ packages/db/lib/errors.ts
@@ -0,0 +1,27 @@
+export class QueryExecutionError extends Error {
+  constructor(
+    public queryName: string,
+    originalError: Error,
+  ) {
+    super(`Error in database query "${queryName}": ${originalError.message}`);
+    this.name = "QueryExecutionError";
+    this.stack = originalError.stack;
+  }
+}
+
+export class DatabaseConnectionError extends Error {
+  constructor(
+    message: string,
+    public originalError?: Error,
+  ) {
+    super(message);
+    this.name = "DatabaseConnectionError";
+  }
+}
+
+export class QueryTimeoutError extends Error {
+  constructor(timeoutDuration: number) {
+    super(`Query timed out after ${timeoutDuration} ms`);
+    this.name = "QueryTimeoutError";
+  }
+}
