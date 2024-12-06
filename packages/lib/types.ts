5075a60ba6f03b558eb3a14a97e4a4db802eec51
Karol <106309115+Karo1808@users.noreply.github.com>
2024-12-06 16:03:29 +0100

    chore(packages/lib): small changes to lib package

--- packages/lib/types.ts
+++ packages/lib/types.ts
@@ -1,4 +1,5 @@
 import type { BaseLogger } from "pino";
+import type { Registry } from "prom-client";
 
 export type NodeEnvironment = "development" | "production" | "test" | "debug";
 export type LogLevel =
@@ -9,4 +10,13 @@ export type LogLevel =
   | "debug"
   | "trace"
   | "silent";
+
 export type Logger = BaseLogger;
+
+export type PrometheusRegistry = Registry;
+
+export type RequireAllExcept<T, K extends keyof T> = Required<Omit<T, K>> &
+  Pick<T, K>;
+
+export type MarkKeysAsPartial<T, K extends keyof T> = Omit<T, K> &
+  Partial<Pick<T, K>>;
