c790685e323a207c8758af009fd09ec02b5930c1
Karol <106309115+Karo1808@users.noreply.github.com>
2024-12-06 16:04:29 +0100

    feat(packages/services): update healthcheckService to include redis connection pinging

--- packages/services/healthcheck.service.ts
+++ packages/services/healthcheck.service.ts
@@ -1,6 +1,21 @@
-import type { Dependencies } from "@novelty/db/lib/types";
 import { getStatusQuery } from "@novelty/db/queries/misc.query";
+import { pingRedisQuery } from "@novelty/redis/queries/index.query";
+import type { MarkKeysAsPartial } from "@novelty/lib/types";
+import type { ServiceDependencies } from "./types";
+import { prepareDependencies } from "./utils";
 
-export const checkDatabaseHealth = async (dependencies: Dependencies) => {
-  return await getStatusQuery(dependencies);
+export const checkDbHealth = async (
+  dependencies: MarkKeysAsPartial<ServiceDependencies, "redisClient">,
+) => {
+  const deps = prepareDependencies(dependencies, "redisClient");
+
+  return await getStatusQuery(deps);
+};
+
+export const checkRedisHealth = async (
+  dependencies: MarkKeysAsPartial<ServiceDependencies, "dbInstance">,
+) => {
+  const deps = prepareDependencies(dependencies, "dbInstance");
+
+  return await pingRedisQuery(deps);
 };
