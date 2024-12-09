d577431ebd67c67619f31f6905e534219b5d6567
Karol <106309115+Karo1808@users.noreply.github.com>
2024-12-09 13:07:01 +0100

    fix(redis): update redis maximum retries

--- packages/redis/index.ts
+++ packages/redis/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable node/no-process-env */
 import { Redis } from "ioredis";
 import "dotenv/config";
+import { configureLogger } from "@novelty/lib/logger";
+import type { NodeEnvironment } from "@novelty/lib/types";
 
 const redisPort = process.env.REDIS_PORT;
 const redisPassword = process.env.REDIS_PASSWORD;
@@ -9,8 +11,30 @@ if (!redisPort || !redisPassword) {
   throw new Error("Missing redis configuration environment variables");
 }
 
+const logger = configureLogger({
+  nodeEnvironment: process.env.NODE_ENV! as NodeEnvironment,
+  hostUrl: "localhost:3000",
+  labels: {
+    source: "redis",
+    environment: process.env.NODE_ENV! as NodeEnvironment,
+  },
+  logLevel: "info",
+});
+
 export const redis = new Redis({
   host: "localhost",
   port: Number(redisPort),
-  maxRetriesPerRequest: 0,
+  maxRetriesPerRequest: null,
+  connectTimeout: 3000,
+  retryStrategy(times) {
+    if (times % 4 === 0) {
+      logger.error({
+        message: "redisRetryError",
+        error: "Redis reconnect exhausted after 3 retries.",
+      });
+      return null;
+    }
+
+    return 200;
+  },
 });
