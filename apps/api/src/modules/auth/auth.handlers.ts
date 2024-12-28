bcc4c9ed6589304a3a15706ddb324642f73baaf7
Karol <106309115+Karo1808@users.noreply.github.com>
2024-12-28 13:36:38 +0100

    feat(api/auth): add new send-email-verification rotute

--- apps/api/src/modules/auth/auth.handlers.ts
+++ apps/api/src/modules/auth/auth.handlers.ts
@@ -1,11 +1,15 @@
 import type { AppRouteHandler } from "@/types/index.types";
-import type { RegisterRoute } from "./auth.routes";
-import { registerUser } from "@novelty/services/auth.service";
+import type { RegisterRoute, SendVerificationEmailRoute } from "./auth.routes";
+import {
+  registerUser,
+  sendVerificationEmail,
+} from "@novelty/services/auth.service";
 import { db } from "@novelty/db";
 import logger from "@/lib/logger";
 import { prometheusRegistry } from "@/lib/metrics";
 import type { ServiceResponse } from "@novelty/services/types";
 import { HttpStatusCodes } from "@novelty/lib/http-status-codes";
+import { redis } from "@novelty/redis";
 
 export const handleRegister: AppRouteHandler<RegisterRoute> = async (c) => {
   const body = c.req.valid("json");
@@ -35,5 +39,51 @@ export const handleRegister: AppRouteHandler<RegisterRoute> = async (c) => {
       user: res.body,
     },
     HttpStatusCodes.CREATED,
+  );
+};
+
+export const handleSendVerificationEmail: AppRouteHandler<
+  SendVerificationEmailRoute
+> = async (c) => {
+  const body = c.req.valid("json");
+
+  const res: ServiceResponse<keyof SendVerificationEmailRoute["responses"]>
+    = await sendVerificationEmail<keyof SendVerificationEmailRoute["responses"]>(
+      {
+        dbInstance: db,
+        redisClient: redis,
+        logger,
+        prometheusRegistry,
+        reqId: c.var.requestId,
+      },
+      body,
+    );
+
+  if (res.status === HttpStatusCodes.NOT_FOUND) {
+    return c.json(
+      {
+        message: "This email does not exist",
+        success: false,
+      },
+      HttpStatusCodes.NOT_FOUND,
+    );
+  }
+
+  if (res.status === HttpStatusCodes.CONFLICT) {
+    return c.json(
+      {
+        message: "This email has already been verified",
+        success: false,
+      },
+      HttpStatusCodes.NOT_FOUND,
+    );
+  }
+
+  return c.json(
+    {
+      message: "Email sent to the recipient",
+      success: true,
+    },
+    HttpStatusCodes.OK,
   );
 };
--- apps/api/src/modules/auth/auth.index.ts
+++ apps/api/src/modules/auth/auth.index.ts
@@ -3,7 +3,9 @@ import { createRouter } from "@/lib/create-app";
 import * as handlers from "./auth.handlers";
 import * as routes from "./auth.routes";
 
-export const authRouter = createRouter().openapi(
-  routes.registerRoute,
-  handlers.handleRegister,
-);
+export const authRouter = createRouter()
+  .openapi(routes.registerRoute, handlers.handleRegister)
+  .openapi(
+    routes.sendVerificationEmailRoute,
+    handlers.handleSendVerificationEmail,
+  );
