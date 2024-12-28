ca36a587a97f0758adaf3dab6fd2c5b6f6ab8b9f
Karol <karolp2003@gmail.com>
2024-12-28 13:38:30 +0100

    feat(redis): add setWithExpirty query

--- packages/redis/queries/index.query.ts
+++ packages/redis/queries/index.query.ts
@@ -1,3 +1,4 @@
+import type { RedisKey, RedisValue } from "ioredis";
 import { createRedisQuery } from "../lib/create-redis-query";
 import type { Dependencies } from "../lib/types";
 
@@ -7,6 +8,21 @@ export const pingRedisQuery = (dependencies: Dependencies) => {
     queryName: "pingRedis",
     query: (redis) => {
       return redis.ping();
+    },
+  });
+};
+
+export const setWithExpiry = (
+  dependencies: Dependencies,
+  key: RedisKey,
+  value: RedisValue,
+  expiryTime: number,
+) => {
+  return createRedisQuery({
+    dependencies,
+    queryName: "setWithExpiryQuery",
+    query: (redis) => {
+      return redis.set(key, value, "EX", expiryTime);
     },
   });
 };
