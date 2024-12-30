fe0b14a6e9389c3e17fa0d3f95b827fb8631d92a
Karol <106309115+Karo1808@users.noreply.github.com>
2024-12-30 15:10:55 +0100

    test(packages/db): create a test-setup file

--- packages/db/queries/test/auth.query.test.ts
+++ packages/db/queries/test/auth.query.test.ts
@@ -1,31 +1,13 @@
-import {
-  afterAll,
-  afterEach,
-  beforeAll,
-  describe,
-  expect,
-  it,
-  vi,
-} from "vitest";
-import type { StartedPostgreSqlContainer } from "@testcontainers/postgresql";
-import { PostgreSqlContainer } from "@testcontainers/postgresql";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { afterEach, describe, expect, it } from "vitest";
 import {
   createUserQuery,
   getIsEmailVerifiedQuery,
   getUserByEmailQuery,
 } from "../auth.query";
-import type { DBClient, Dependencies } from "../../lib/types";
-import { configureLogger } from "@novelty/lib/logger";
-import { Pool } from "pg";
-import { Registry } from "prom-client";
-import type { Pool as TPool } from "pg";
-import path from "node:path";
-import * as schema from "../../schemas/index.schema";
 import "dotenv/config";
-import { migrate } from "drizzle-orm/node-postgres/migrator";
 import { usersTable } from "../../schemas/user.schema";
 import { eq } from "drizzle-orm";
+import { testDb, testDependencies } from "test-setup";
 
 // eslint-disable-next-line node/no-process-env
 if (process.env.NODE_ENV !== "test") {
@@ -33,49 +15,6 @@ if (process.env.NODE_ENV !== "test") {
 }
 
 describe("auth queries", () => {
-  let container: StartedPostgreSqlContainer;
-  let pool: TPool;
-  let dbClient: DBClient;
-  let dependencies: Dependencies;
-
-  beforeAll(async () => {
-    container = await new PostgreSqlContainer()
-      .withStartupTimeout(12000)
-      .start();
-
-    pool = new Pool({
-      connectionString: container.getConnectionUri(),
-    });
-
-    dbClient = drizzle({ client: pool, schema });
-
-    const migrationsFolder = path.resolve(__dirname, "../../migrations");
-
-    await migrate(dbClient, {
-      migrationsFolder,
-    });
-
-    const logger = configureLogger({
-      nodeEnvironment: "test",
-      hostUrl: "",
-      labels: {},
-      logLevel: "info",
-    });
-
-    dependencies = {
-      dbInstance: dbClient,
-      logger,
-      reqId: "test-req-id",
-      prometheusRegistry: new Registry(),
-    };
-  });
-
-  afterAll(async () => {
-    await pool.end();
-    await container.stop();
-    vi.clearAllMocks();
-  });
-
   describe("getUserByEmailQuery", () => {
     const dummyUser = {
       email: "mail@email.com",
@@ -85,12 +24,12 @@ describe("auth queries", () => {
     it("should find and returns the user by email", async () => {
       const startTime = Date.now();
 
-      await dbClient.insert(usersTable).values(dummyUser);
+      await testDb.insert(usersTable).values(dummyUser);
 
       const result = await getUserByEmailQuery(
         {
-          ...dependencies,
-          dbInstance: dbClient,
+          ...testDependencies,
+          dbInstance: testDb,
         },
         dummyUser.email,
       );
@@ -113,13 +52,13 @@ describe("auth queries", () => {
         endTime,
       );
 
-      await dbClient
+      await testDb
         .delete(usersTable)
         .where(eq(usersTable.email, dummyUser.email));
     });
 
     it("should return undefined when user does not exist", async () => {
-      const res = await getUserByEmailQuery(dependencies, dummyUser.email);
+      const res = await getUserByEmailQuery(testDependencies, dummyUser.email);
 
       expect(res).toBeUndefined();
     });
@@ -132,7 +71,7 @@ describe("auth queries", () => {
     };
 
     afterEach(async () => {
-      await dbClient
+      await testDb
         .delete(usersTable)
         .where(eq(usersTable.email, dummyUser.email));
     });
@@ -140,7 +79,7 @@ describe("auth queries", () => {
     it("should create user if they don't exist", async () => {
       const startTime = Date.now();
 
-      const [result] = await createUserQuery(dependencies, dummyUser);
+      const [result] = await createUserQuery(testDependencies, dummyUser);
 
       const endTime = Date.now();
 
@@ -162,10 +101,10 @@ describe("auth queries", () => {
     });
 
     it("should throw error if the user already exists", async () => {
-      await dbClient.insert(usersTable).values(dummyUser);
+      await testDb.insert(usersTable).values(dummyUser);
 
       await expect(
-        createUserQuery(dependencies, dummyUser),
+        createUserQuery(testDependencies, dummyUser),
       ).rejects.toMatchObject({
         message: expect.stringContaining(
           "duplicate key value violates unique constraint",
@@ -182,12 +121,12 @@ describe("auth queries", () => {
     };
 
     it("should return the isEmailVerified column value", async () => {
-      await dbClient.insert(usersTable).values(dummyUser);
+      await testDb.insert(usersTable).values(dummyUser);
 
       const result = await getIsEmailVerifiedQuery(
         {
-          ...dependencies,
-          dbInstance: dbClient,
+          ...testDependencies,
+          dbInstance: testDb,
         },
         dummyUser.email,
       );
@@ -198,13 +137,16 @@ describe("auth queries", () => {
       });
       expect(result).not.toHaveProperty("password");
 
-      await dbClient
+      await testDb
         .delete(usersTable)
         .where(eq(usersTable.email, dummyUser.email));
     });
 
     it("should return undefined when user does not exist", async () => {
-      const res = await getIsEmailVerifiedQuery(dependencies, dummyUser.email);
+      const res = await getIsEmailVerifiedQuery(
+        testDependencies,
+        dummyUser.email,
+      );
 
       expect(res).toBeUndefined();
     });
