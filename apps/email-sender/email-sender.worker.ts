a118fedb0717aa92277c95b3a26439c969f59c2e
Karol <karolp2003@gmail.com>
2025-02-11 11:26:15 +0100

    chore(apps/email-sender): update woker and processor

--- apps/email-sender/email-sender.worker.ts
+++ apps/email-sender/email-sender.worker.ts
@@ -1,72 +1,11 @@
-import type { Job } from "bullmq";
-import { Worker } from "bullmq";
-import { redisConfig } from "@novelty/message-queue/config/index";
-import { logger } from "./lib/logger";
-import { captureException } from "@novelty/lib/sentry";
-import { processVerificationEmail } from "./email-sender.handler";
+import { processVerificationEmail } from "@novelty/message-queue/queues/email.handler";
+import { createWorker } from "@novelty/message-queue/lib/create-worker";
+import env from "./lib/env";
 
-const jobProcessors: Record<string, (data: any) => Promise<void>> = {
-  "send-verification-email": processVerificationEmail,
+export const jobProcessors: Record<string, (data: any) => Promise<void>> = {
+  "send-verification-email": async (data) => {
+    processVerificationEmail(data, env.SENDER_EMAIL);
+  },
 };
 
-const emailWorker = new Worker(
-  "email-queue",
-  async (job: Job) => {
-    const processor = jobProcessors[job.name];
-    if (!processor) {
-      throw new Error(`No processor defined for job type: ${job.name}`);
-    }
-    await processor(job.data);
-  },
-  {
-    connection: redisConfig,
-  },
-);
-
-emailWorker.on("completed", (job, result) => {
-  logger.info({
-    message: `Job:${job.name}] Job ${job?.id} completed.`,
-    jobName: job.name,
-    jobId: job.id,
-    status: "completed",
-    result,
-  });
-});
-
-emailWorker.on("failed", (job, error) => {
-  logger.error({
-    message: `Job: ${job?.name} Job ${job?.id} failed: ${error.message}`,
-    jobName: job?.name,
-    jobId: job?.id,
-    status: "failure",
-    error,
-  });
-  captureException({
-    error,
-    tags: [{ name: "jobId", value: job?.id }],
-    breadcrumb: {
-      category: "worker process",
-      message: error.message,
-      level: "error",
-    },
-    contextName: job?.name,
-    context: { jobName: job?.name },
-  });
-});
-
-emailWorker.on("error", (err) => {
-  logger.error({
-    message: "Unexpected worker error",
-    status: "error",
-    error: err,
-  });
-
-  captureException({
-    error: err,
-    breadcrumb: {
-      category: "worker process",
-      message: err.message,
-      level: "error",
-    },
-  });
-});
+export const emailWorker = createWorker("email-queue", jobProcessors);
