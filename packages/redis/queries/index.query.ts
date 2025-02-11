a97940e359cfe4d10a7f2e218a4c83c15528634d
Karol <106309115+Karo1808@users.noreply.github.com>
2025-02-11 11:23:46 +0100

    feat(redis/queries): add deleteByKey query

--- packages/redis/queries/index.query.ts
+++ packages/redis/queries/index.query.ts
@@ -27,6 +27,16 @@ export const setWithExpiry = (
   });
 };
 
+export const deleteByKey = (dependencies: Dependencies, key: RedisKey) => {
+  return createRedisQuery({
+    dependencies,
+    queryName: "deleteByKey",
+    query: async (redis) => {
+      return await redis.del(key);
+    },
+  });
+};
+
 export const acquireLock = (
   dependencies: Dependencies,
   key: RedisKey,
