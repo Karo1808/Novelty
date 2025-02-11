3b907d4ce1fefaa6111027ab6e619e97ceecdb64
Karol <106309115+Karo1808@users.noreply.github.com>
2025-02-11 11:40:20 +0100

    test(services/auth): test sendVerificationEmail

--- packages/services/test-setup.ts
+++ packages/services/test-setup.ts
@@ -1,30 +1,29 @@
 /* eslint-disable import/no-mutable-exports */
 import path from "node:path";
 import * as schema from "@novelty/db/schemas/index.schema";
-import {
-  PostgreSqlContainer,
-  type StartedPostgreSqlContainer,
-} from "@testcontainers/postgresql";
+import { PostgreSqlContainer } from "@testcontainers/postgresql";
+import type { StartedPostgreSqlContainer } from "@testcontainers/postgresql";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { afterAll, beforeAll, expect, vi } from "vitest";
-import { Pool, type Pool as TPool } from "pg";
+import { afterAll, beforeAll, vi } from "vitest";
+import { Pool } from "pg";
+import type { Pool as TPool } from "pg";
 import type { DBClient } from "@novelty/db/lib/types";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { configureLogger } from "@novelty/lib/logger";
 import { Registry } from "prom-client";
 import type { ServiceDependencies } from "types";
-import { Redis, type Redis as TRedis } from "ioredis";
+import { Redis } from "ioredis";
+import type { Redis as TRedis } from "ioredis";
 import type { StartedRedisContainer } from "@testcontainers/redis";
 import { RedisContainer } from "@testcontainers/redis";
-import * as matchers from "@testing-library/jest-dom/matchers";
+import { createQueue } from "@novelty/message-queue/lib/create-queue";
+import type { Queue } from "bullmq";
+import { createWorker } from "@novelty/message-queue/lib/create-worker";
 
 // eslint-disable-next-line node/no-process-env
 if (process.env.NODE_ENV !== "test") {
   throw new Error("NODE_ENV must be 'test'");
 }
-
-// Extend Vitest's expect with jest-dom matchers
-expect.extend(matchers);
 
 let dbContainer: StartedPostgreSqlContainer;
 let redisContainer: StartedRedisContainer;
@@ -32,18 +31,36 @@ let pool: TPool;
 let testDb: DBClient;
 let testRedis: TRedis;
 let testDependencies: ServiceDependencies;
+let testDependenciesWithQueue: Required<ServiceDependencies>;
+let testQueue: Queue;
 
 beforeAll(async () => {
   dbContainer = await new PostgreSqlContainer()
     .withStartupTimeout(12000)
     .start();
+
   redisContainer = await new RedisContainer().start();
 
   testRedis = new Redis(redisContainer.getConnectionUrl());
 
+  const connectionOptions = {
+    host: redisContainer.getHost(),
+    port: redisContainer.getPort(),
+  };
+
+  const queueName = "email-queue";
+  const jobProcessors: Record<string, (data: any) => Promise<void>> = {
+    "send-verification-email": async () => {},
+  };
+
+  testQueue = createQueue(queueName, connectionOptions);
+
+  createWorker(queueName, jobProcessors, connectionOptions);
+
   pool = new Pool({
     connectionString: dbContainer.getConnectionUri(),
   });
+
   testDb = drizzle({ client: pool, schema });
 
   const migrationsFolder = path.resolve(__dirname, "../db/migrations");
@@ -65,6 +82,15 @@ beforeAll(async () => {
     prometheusRegistry: new Registry(),
     redisClient: testRedis,
   };
+
+  testDependenciesWithQueue = {
+    dbInstance: testDb,
+    logger,
+    reqId: "test-req-id",
+    prometheusRegistry: new Registry(),
+    redisClient: testRedis,
+    messageQueueInstance: testQueue,
+  };
 });
 
 afterAll(async () => {
@@ -74,4 +100,10 @@ afterAll(async () => {
   vi.clearAllMocks();
 });
 
-export { testDb, testDependencies, testRedis };
+export {
+  testDb,
+  testDependencies,
+  testDependenciesWithQueue,
+  testQueue,
+  testRedis,
+};
