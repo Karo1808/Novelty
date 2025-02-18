b37bed483e341ca1c9b9b014964671dd26b3d55f
Karol <karolp2003@gmail.com>
2025-02-18 16:31:13 +0100

    feat(/packages/db): rename user_profile schema to user_info

--- packages/db/migrations/0002_empty_colossus.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"id" varchar(255) PRIMARY KEY NOT NULL,
-	"email" varchar(255) NOT NULL,
-	"password" varchar(255) NOT NULL,
-	"is_email_verified" boolean NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp NOT NULL,
-	CONSTRAINT "users_email_unique" UNIQUE("email")
-);
