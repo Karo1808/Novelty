4d132f603ba1f7595d127affe28bbad04dedc594
Karol <karolp2003@gmail.com>
2025-06-18 15:47:27 +0200

    chore(server to client): connect rpc to web app

---
+++ apps/web/.output/server/chunks/_/_tanstack-start-manifest_v-B3OYdlEj.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "/home/karo/Documents/Code/my-projects/Novelty/apps/web/src/routes/__root.tsx", "children": ["/"], "preloads": ["/assets/main-D3RT35Xj.js"], "assets": [{ "tag": "script", "attrs": { "src": "/assets/main-D3RT35Xj.js", "type": "module" } }] }, "/": { "filePath": "/home/karo/Documents/Code/my-projects/Novelty/apps/web/src/routes/index.tsx", "assets": [], "preloads": ["/assets/index-Diczsq7v.js"] } } });
+
+export { tsrStartManifest };
+//# sourceMappingURL=_tanstack-start-manifest_v-B3OYdlEj.mjs.map
