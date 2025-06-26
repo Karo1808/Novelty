1f106c883a4462c00bcd86957380d3c05b4c82cf
Karol <106309115+Karo1808@users.noreply.github.com>
2025-06-26 15:49:19 +0200

    feat(api/user): add GET /user route

--- apps/api/src/modules/user/user.index.ts
+++ apps/api/src/modules/user/user.index.ts
@@ -3,6 +3,7 @@ import * as handlers from "./user.handlers";
 import * as routes from "./user.routes";
 
 export const userRouter = createRouter()
+  .openapi(routes.getUserRoute, handlers.handleGetUser)
   .openapi(routes.getProfileRoute, handlers.handleGetProfile)
   .openapi(routes.updateProfileRoute, handlers.handleUpdateProfile)
   .openapi(routes.getPreferencesRoute, handlers.handleGetPreferences)
